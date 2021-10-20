ca87a40791a4b3e32f189648e58d9ee9a4aee571
Kyle Mathews <mathews.kyle@gmail.com>
2021-10-20 14:25:03 -0700

    Actually query id

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       {
         allContentfulBlogPost {
           nodes {
+            id
             title
             slug
           }
